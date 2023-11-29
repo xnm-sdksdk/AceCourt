0422790f9aafc98ba8ff61348e268fd077379b21
xnm-sdksdk <n.mendonca@protonmail.com>
2023-11-29 23:40:01 +0000

    main js

--- src/main.js
+++ src/main.js
@@ -10,12 +10,12 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+const app = createApp(App);
+
 const vuetify = createVuetify({
   components,
   directives,
 });
-
-const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
