1a610d09d8e98ccf1713e01c2b4e59832dbe0e87
xnm-sdksdk <n.mendonca@protonmail.com>
2023-11-29 23:42:03 +0000

    first skeleton signUp

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-card>
+            <v-card-title>Sign Up</v-card-title>
+            <v-card-text>
+              <v-form @submit.prevent="createAccount"> </v-form>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    createAccount() {},
+  },
+};
+</script>
