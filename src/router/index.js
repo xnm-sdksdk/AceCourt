913dc8e08255e2d6b1ff2b8eee52598394bde3e7
xnm-sdksdk <n.mendonca@protonmail.com>
2023-12-17 00:00:34 +0000

    routes fixed

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
-import DrawsView from "../views/DrawsView.vue";
-import EditProfile from "../views/EditProfileView.vue";
+import LandingPageView from "@/views/LandingPageView.vue";
+import SignUpView from "@/views/SignUpView.vue";
+import LogInView from "@/views/LogInView.vue";
 import HomePageView from "../views/HomePageView.vue";
-import LandingPageView from "../views/LandingPageView.vue";
-import LogInView from "../views/LogInView.vue";
-import MatchReportView from "../views/MatchReportView.vue";
-import MyStatsView from "../views/MyStatsView.vue";
-import NewsView from "../views/NewsView.vue";
-import PlayersView from "../views/PlayersView.vue";
-import ProfileView from "../views/ProfileView.vue";
-import RankingView from "../views/RankingView.vue";
-import ScheduleView from "../views/ScheduleView.vue";
-import ScoresView from "../views/ScoresView.vue";
-import SignUpView from "../views/SignUpView.vue";
-//import TrophiesView from "../views/TrophyView.vue";
-import UpComingGamesView from "../views/UpComingGamesView.vue";
-import PageNotFoundView from "../views/PageNotFoundView.vue";
-// import { useUserStore } from "@/stores/user";
+import ScoresView from "@/views/ScoresView.vue";
+import RankingView from "@/views/RankingView.vue";
+import PlayersView from "@/views/PlayersView.vue";
+import DrawsView from "@/views/DrawsView.vue";
+import ScheduleView from "@/views/ScheduleView.vue";
+import NewsView from "@/views/NewsView.vue";
+import MatchReportView from "@/views/MatchReportView.vue";
+import UpcomingGamesView from "@/views/UpcomingGamesView.vue";
+import ProfileView from "@/views/ProfileView.vue";
+import MyStatsView from "@/views/MyStatsView.vue";
+import EditProfile from "@/views/EditProfileView.vue";
+import TrophiesView from "@/views/TrophiesView.vue";
+import PageNotFoundView from "@/views/PageNotFoundView.vue";
+import { useUserStore } from "@/stores/user";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,16 +25,137 @@ const router = createRouter({
       path: "/",
       name: "landingpage",
       component: LandingPageView,
+      meta: {
+        title: "Ace Court",
+      },
+    },
+    {
+      path: "/home",
+      name: "home",
+      component: HomePageView,
+      meta: {
+        title: "HomePage",
+      },
     },
     {
       path: "/login",
       name: "login",
       component: LogInView,
+      meta: {
+        title: "Login",
+      },
+    },
+    {
+      path: "/signup",
+      name: "signup",
+      component: SignUpView,
+      meta: {
+        title: "Sign Up",
+      },
+    },
+    {
+      path: "/scores",
+      name: "scores",
+      component: ScoresView,
+      meta: {
+        title: "Scores",
+      },
+    },
+    {
+      path: "/draws",
+      name: "draw",
+      component: DrawsView,
+      meta: {
+        title: "Draws",
+      },
+    },
+    {
+      path: "/ranking",
+      name: "ranking",
+      component: RankingView,
+      meta: {
+        title: "Ranking",
+      },
+    },
+    {
+      path: "/players",
+      name: "players",
+      component: PlayersView,
+      meta: {
+        title: "Players",
+      },
+    },
+    {
+      path: "/schedule",
+      name: "schedule",
+      component: ScheduleView,
+      meta: {
+        title: "Schedule",
+      },
+    },
+    {
+      path: "/news",
+      name: "news",
+      component: NewsView,
+      meta: {
+        title: "News",
+      },
+    },
+    {
+      path: "/matchreport",
+      name: "matchreport",
+      component: MatchReportView,
+      meta: {
+        title: "Match Report",
+      },
+    },
+    {
+      path: "/upcominggames",
+      name: "upcominggames",
+      component: UpcomingGamesView,
+      meta: {
+        title: "Upcoming Games",
+      },
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: ProfileView,
+      meta: {
+        title: "Profile",
+      },
+    },
+    {
+      path: "/mystats",
+      name: "mystats",
+      component: MyStatsView,
+      meta: {
+        title: "My Stats",
+      },
+    },
+    {
+      path: "/edit",
+      name: "edit",
+      component: EditProfile,
+      meta: {
+        title: "Edit Profile",
+      },
+    },
+    {
+      path: "/trophies",
+      name: "trophies",
+      component: TrophiesView,
+      meta: {
+        title: "Trophies",
+      },
     },
     {
       path: "/:PageNotFound(.*)*",
       name: "pagenotfound",
       component: PageNotFoundView,
+      meta: {
+        title: "PageNotFound",
+      },
     },
   ],
 });
@@ -51,6 +172,13 @@ const router = createRouter({
 //       query: { redirect: to.fullPath },
 //     };
 //   }
+// });
+
+// router.beforeEach((to, from, next) => {
+//   if (to.meta.title) {
+//     document.title = to.meta.title;
+//   }
+//   next();
 // });
 
 export default router;
