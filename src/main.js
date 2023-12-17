3681f1e9668da875c89430f9c79bf0a7efc6acc8
xnm-sdksdk <n.mendonca@protonmail.com>
2023-12-17 00:00:29 +0000

    pinia state

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+// Pinia Persisted State
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import App from "./App.vue";
 import router from "./router";
 // Vuetify
@@ -17,7 +19,10 @@ const vuetify = createVuetify({
   directives,
 });
 
-app.use(createPinia());
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+app.use(pinia);
 app.use(router);
 app.use(vuetify);
 
