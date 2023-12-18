9ae2bd433f3d82ce24a6aa0acdb268866a0f9ebd
xnm-sdksdk <n.mendonca@protonmail.com>
2023-12-18 13:32:51 +0000

    Component Button See More

---
+++ src/components/ButtonSeeMore.vue
@@ -0,0 +1,18 @@
+<template>
+  <button type="button" class="p-3 m-3 rounded-3xl bg-lime-400 text-white">
+    {{ btnSeeMoreText }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: { type: String },
+  },
+  computed: {
+    btnSeeMoreText() {
+      return this.text;
+    },
+  },
+};
+</script>
