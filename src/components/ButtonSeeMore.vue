3e3b6dbdbd5f0cc7b69ef0e4f75fcb26e4948775
xnm-sdksdk <n.mendonca@protonmail.com>
2023-12-19 10:09:08 +0000

    styling button

--- src/components/ButtonSeeMore.vue
+++ src/components/ButtonSeeMore.vue
@@ -1,5 +1,5 @@
 <template>
-  <button type="button" class="w-32 w-full p-3 m-3 uppercase font-semibold tracking-wide rounded-3xl bg-lime-400 text-white">
+  <button type="button" class="w-32 p-3 m-3 uppercase font-semibold tracking-wide rounded-3xl bg-lime-400 text-white">
     {{ btnSeeMoreText }}
   </button>
 </template>
