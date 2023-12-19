14eede3a4b007c08b1bcd32b34325828beddd487
xnm-sdksdk <n.mendonca@protonmail.com>
2023-12-19 10:08:55 +0000

    import vuetify

--- src/main.js
+++ src/main.js
@@ -6,13 +6,24 @@ import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import App from "./App.vue";
 import router from "./router";
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 const app = createApp(App);
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
 app.use(pinia);
+app.use(vuetify);
 app.use(router);
 
 app.mount("#app");
