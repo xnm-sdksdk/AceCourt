58b822019fb6b648bafb3bd273e6e3fcb6debce1
FredSilva21 <fredsilva43@gmail.com>
2023-12-20 12:16:28 +0000

    add second container

--- src/views/LandingPageView.vue
+++ src/views/LandingPageView.vue
@@ -1,43 +1,69 @@
 <template>
   <div>
     <NavBar></NavBar>
-    <v-container class="mt-5">
-      <v-row>
-        <v-col cols="12" md="6" class="align-self-center">
-          <!-- Title -->
-          <h1 class="text-5xl font-bold mb-4">
-            Discover Australian Open Like Never Before
-          </h1>
+    <main>
+      <v-container class="mt-5">
+        <v-row>
+          <v-col cols="12" md="6" class="align-self-center">
+            <!-- Title -->
+            <h1 class="text-5xl font-bold mb-4">
+              Discover Australian Open Like Never Before
+            </h1>
 
-          <!-- Description -->
-          <p class="text-lg mb-4">
-            Welcome to Ace Court, your gateway to an immersive tennis
-            experience. Get real-time updates, personalize your favorites, stay
-            informed with updated news, and explore player rankings—all in one
-            place. Elevate your love for tennis with us.
-          </p>
+            <!-- Description -->
+            <p class="text-lg mb-4">
+              Welcome to Ace Court, your gateway to an immersive tennis
+              experience. Get real-time updates, personalize your favorites,
+              stay informed with updated news, and explore player rankings—all
+              in one place. Elevate your love for tennis with us.
+            </p>
 
-          <!-- Start Button -->
-          <RouterLink :to="{ name: 'signup' }">
-            <button
-              class="text-white px-4 py-2 rounded-full"
-              style="letter-spacing: 0.15em"
-            >
-              Start
-            </button>
-          </RouterLink>
-        </v-col>
+            <!-- Start Button -->
+            <RouterLink :to="{ name: 'signup' }">
+              <button
+                class="text-white px-4 py-2 rounded-full"
+                style="letter-spacing: 0.15em"
+              >
+                Start
+              </button>
+            </RouterLink>
+          </v-col>
 
-        <!-- Image -->
-        <v-col cols="12" md="6">
-          <img
-            src="@/assets/HeroImage.svg"
-            alt="Imagem"
-            class="w-full h-auto small-image"
-          />
-        </v-col>
-      </v-row>
-    </v-container>
+          <!-- Image -->
+          <v-col cols="12" md="6">
+            <img
+              src="@/assets/HeroImage.svg"
+              alt="Imagem"
+              class="w-full h-auto small-image"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <v-container class="mt-5" style="background-color: #efefef">
+        <v-row>
+          <v-col cols="12" md="4" v-for="index in 3" :key="index">
+            <v-card>
+              <!--Icon-->
+              <img
+                src="../assets/liveGames.svg"
+                class="icon-image"
+                style="max-height: 75px"
+              />
+
+              <!-- Title -->
+              <v-card-title class="mt-0">Real-Time Updates</v-card-title>
+
+              <!-- Description -->
+              <v-card-text>
+                Get instant updates on match outcomes, scores, and key moments
+                as they happen on the court.
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </main>
     <Footer></Footer>
   </div>
 </template>
@@ -74,8 +100,12 @@ button {
   transition: background-color 0.5s ease-out;
 }
 
-button:hover{
-  background-color: #008BCC;
+button:hover {
+  background-color: #008bcc;
   transition: background-color 0.5s ease-out;
+}
+.icon-image {
+  width: 100%;
+  height: auto;
 }
 </style>
