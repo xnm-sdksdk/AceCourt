93fcbc23ffcc20295de0e5f93f4d2eae3fcf755b
xnm-sdksdk <n.mendonca@protonmail.com>
2023-12-20 11:20:36 +0000

    images routes styling

--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -7,21 +7,26 @@
         <v-row>
           <v-col>
             <v-card class="rounded-b-xl" elevation="3">
-              <!-- <v-img
-                src=""
-                alt="Highlight-Player-Image"
-                width="150"
-                height="150"
-              >
-              </v-img> -->
               <v-card-title class="ml-4"> Highlight Player </v-card-title>
+
+              <v-avatar size="100">
+                <v-img
+                  src="../public/assets/carlos.svg"
+                  alt="Highlight-Player-Image"
+                  width="200"
+                  height="200"
+                >
+                </v-img>
+              </v-avatar>
               <v-card-subtitle class="ml-4">Carlos Alcaraz</v-card-subtitle>
 
               <v-card-text class="ml-4">
                 Spanish tennis sensation Carlos Alcaraz showcases formidable
                 skill and remarkable achievements.
               </v-card-text>
-              <ButtonSeeMore :text="SeeMore"></ButtonSeeMore>
+              <RouterLink :to="{ name: 'players' }">
+                <ButtonSeeMore :text="SeeMore"></ButtonSeeMore>
+              </RouterLink>
             </v-card>
           </v-col>
 
@@ -30,6 +35,7 @@
             <div class="">
               <v-card class="rounded-b-xl" elevation="3">
                 <v-card-title class="ml-4"> Live Games </v-card-title>
+
                 <v-card-subtitle class="ml-4">Real-Time Data</v-card-subtitle>
 
                 <v-card-text class="ml-4">
@@ -37,7 +43,9 @@
                   dashboard, providing real-time data for an immersive gaming
                   experience
                 </v-card-text>
-                <ButtonSeeMore :text="SeeMore"></ButtonSeeMore>
+                <RouterLink :to="{ name: 'scores' }">
+                  <ButtonSeeMore :text="SeeMore"></ButtonSeeMore>
+                </RouterLink>
               </v-card>
             </div>
           </v-col>
@@ -50,13 +58,27 @@
         <v-row>
           <v-col>
             <v-card class="rounded-b-xl" elevation="3">
-              <!-- Carlos -->
-              <!-- <v-img src="" alt="Carlos-Alcaraz-Image" width="150" height="150">
-          </v-img> -->
-              <!-- Jannik -->
-              <!-- <v-img src="" alt="Jannik-Sinner-Image" width="150" height="150">
-          </v-img> -->
               <v-card-title class="ml-4"> Top Matches </v-card-title>
+              <!-- Carlos -->
+              <v-avatar size="100">
+                <v-img
+                  src="../public/assets/carlos.svg"
+                  alt="Carlos-Alcaraz-Image"
+                  width="200"
+                  height="2050"
+                >
+                </v-img>
+              </v-avatar>
+              <!-- Jannik -->
+              <v-avatar size="100">
+                <v-img
+                  src="../public/assets/jannik.svg"
+                  alt="Jannik-Sinner-Image"
+                  width="200"
+                  height="200"
+                >
+                </v-img>
+              </v-avatar>
               <v-card-subtitle class="ml-4"
                 >Jannik Sinner defeats Carlos Alcaraz in a perfect
                 Game</v-card-subtitle
@@ -68,15 +90,24 @@
             <!-- News Card -->
             <div class="">
               <v-card class="rounded-b-xl" elevation="3">
-                <!-- Player -->
-                <!-- <v-img src="" alt="Player-Image" width="150" height="150">
-          </v-img> -->
                 <v-card-title class="ml-4"> News </v-card-title>
+                <!-- Player -->
+                <v-avatar size="100">
+                  <v-img
+                    src="../public/assets/player.svg"
+                    alt="Player-Image"
+                    width="200"
+                    height="200"
+                  >
+                  </v-img>
+                </v-avatar>
                 <v-card-subtitle class="ml-4"
                   >Tennis - "We'll be back", say Australia's Davis Cup
                   nearly-men</v-card-subtitle
                 >
-                <ButtonSeeMore :text="SeeMore"></ButtonSeeMore>
+                <RouterLink :to="{ name: 'news' }">
+                  <ButtonSeeMore :text="SeeMore"></ButtonSeeMore>
+                </RouterLink>
               </v-card>
             </div>
           </v-col>
@@ -87,28 +118,51 @@
         <v-row>
           <v-col>
             <v-card class="rounded-b-xl" elevation="3">
-              <!-- Player -->
-              <!-- <v-img src="" alt="Djokovic-Ranking-Image" width="150" height="150">
-          </v-img> -->
               <v-card-title class="ml-4"> Ranking </v-card-title>
+              <!-- Player -->
+              <v-avatar size="200">
+                <v-img
+                  src="../public/assets/nova.svg"
+                  alt="Djokovic-Ranking-Image"
+                  width="300"
+                  height="300"
+                >
+                </v-img>
+              </v-avatar>
               <v-card-subtitle class="ml-4"
                 >Tennis - "We'll be back", say Australia's Davis Cup
                 nearly-men</v-card-subtitle
               >
-              <ButtonSeeMore :text="SeeMore"></ButtonSeeMore>
+              <RouterLink :to="{ name: 'ranking' }">
+                <ButtonSeeMore :text="SeeMore"></ButtonSeeMore>
+              </RouterLink>
             </v-card>
           </v-col>
           <v-col>
             <!-- Head to Head Card -->
             <div class="">
               <v-card class="rounded-b-xl" elevation="3">
-                <!-- De Minaur -->
-                <!-- <v-img src="" alt="De-Minaur-Image" width="150" height="150">
-          </v-img> -->
-                <!-- Medvedev -->
-                <!-- <v-img src="" alt="Medvedev-Image" width="150" height="150">
-          </v-img> -->
                 <v-card-title class="ml-4"> Head to Head </v-card-title>
+                <!-- De Minaur -->
+                <v-avatar size="100">
+                  <v-img
+                    src="../public/assets/alex.svg"
+                    alt="De-Minaur-Image"
+                    width="150"
+                    height="150"
+                  >
+                  </v-img>
+                </v-avatar>
+                <!-- Medvedev  -->
+                <v-avatar size="100">
+                  <v-img
+                    src="../public/assets/medvedev.svg"
+                    alt="Medvedev-Image"
+                    width="150"
+                    height="150"
+                  >
+                  </v-img>
+                </v-avatar>
                 <v-card-subtitle class="ml-4"
                   >Medvedev gets revenge on De Minaur and return to US Open
                   quarter-finals</v-card-subtitle
