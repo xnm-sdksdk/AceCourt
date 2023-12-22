58ae6779555189d3e5de545afc606bdab54a3206
FredSilva21 <fredsilva43@gmail.com>
2023-12-22 12:54:28 +0000

    start ranking

--- src/views/RankingView.vue
+++ src/views/RankingView.vue
@@ -1,18 +1,87 @@
 <template>
-  <NavBar></NavBar>
-  <div class="">
-    <h1>Ranking</h1>
+  <div>
+    <NavBar></NavBar>
+
+    <v-container>
+      <!-- Header Section -->
+      <v-row>
+        <v-col>
+          <h1>Ranking</h1>
+        </v-col>
+        <v-col class="text-right">
+          <button
+            style="letter-spacing: 0.15em"
+            @click="goBack"
+          >
+            <img src="@/assets/arrowSelect.svg" alt="">
+            Back
+          </button>
+        </v-col>
+      </v-row>
+
+      <!-- Select Section -->
+      <v-row>
+        <v-col>
+          <v-select
+            clearable
+            label="Select"
+            :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+            full-width
+          ></v-select>
+        </v-col>
+      </v-row>
+
+      <!-- Table Section -->
+      <v-row>
+        <v-col>
+          <v-data-table
+            :items="desserts"
+            :headers="headers"
+            :items-per-page="5"
+            class="elevation-1"
+          >
+            <template v-slot:item="{ item }">
+              <tr>
+                <td>{{ item.position }}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.nationality }}</td>
+                <td>{{ item.wl }}</td>
+                <td>{{ item.points }}</td>
+              </tr>
+            </template>
+          </v-data-table>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
+import {useGameStore} from "@/stores/tennis.js"
 export default {
   components: {
     NavBar,
   },
-  // setup() {},
+  data() {
+    return {
+      store:useGameStore(),
+      players:[]
+    };
+  },
+
+  created () {
+    this.store.fetchPlayers();
+    this.players = this.store.getPlayers || [];
+    console.log(this.players);
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+</style>
