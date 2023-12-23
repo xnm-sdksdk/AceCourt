f4066d0d72135dc09a1497d486bf224b1559ee83
xnm-sdksdk <n.mendonca@protonmail.com>
2023-12-23 13:11:12 +0000

    component for scores

---
+++ src/components/ScoresHeaderResults.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-row class="rounded-t-xl bg-white ma-1">
+    <v-col>Results</v-col>
+    <v-col>Icon</v-col>
+    <v-col>1st</v-col>
+    <v-col>2nd</v-col>
+    <v-col>3rd</v-col>
+    <v-col>4th</v-col>
+    <v-col>5th</v-col>
+    <v-col>Sets</v-col>
+  </v-row>
+</template>
