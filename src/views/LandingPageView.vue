7495233b2a09ed4330455ce0f951edde08795bee
FredSilva21 <fredsilva43@gmail.com>
2023-12-27 15:36:32 +0000

    adjust landing

--- src/views/LandingPageView.vue
+++ src/views/LandingPageView.vue
@@ -1,289 +1,278 @@
 <template>
   <div>
     <NavBar></NavBar>
-    <main>
-      <v-container
-        class="container mx-auto flex justify-between items-center p-4"
-      >
-        <v-row>
-          <v-col cols="12" md="6" class="align-self-center">
+    <v-container
+      class="container mx-auto flex justify-between items-center p-4"
+    >
+      <v-row>
+        <v-col cols="12" md="6" class="align-self-center">
+          <!-- Title -->
+          <h1 class="text-5xl font-bold mb-4">
+            Discover Australian Open Like Never Before
+          </h1>
+
+          <!-- Description -->
+          <p class="text-lg mb-4">
+            Welcome to Ace Court, your gateway to an immersive tennis
+            experience. Get real-time updates, personalize your favorites, stay
+            informed with updated news, and explore player rankings—all in one
+            place. Elevate your love for tennis with us.
+          </p>
+
+          <!-- Start Button -->
+          <RouterLink :to="{ name: 'home' }">
+            <button
+              class="text-white px-4 py-2 rounded-full"
+              style="letter-spacing: 0.15em"
+            >
+              Start
+            </button>
+          </RouterLink>
+        </v-col>
+
+        <!-- Image -->
+        <v-col cols="12" md="6" class="d-flex align-center justify-center">
+          <img
+            src="@/assets/HeroImage.svg"
+            alt="Imagem"
+            class="w-full h-auto small-image"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container class="mt-10" style="background-color: #efefef; width: 100%">
+      <v-row class="fill-height">
+        <v-col cols="12" md="4">
+          <div class="feature-container">
+            <!--Icon-->
+            <img src="../assets/liveGames.svg" class="icon-image" />
+
             <!-- Title -->
-            <h1 class="text-5xl font-bold mb-4">
-              Discover Australian Open Like Never Before
-            </h1>
+            <h2 class="feature-title">Real-Time Updates</h2>
 
             <!-- Description -->
-            <p class="text-lg mb-4">
-              Welcome to Ace Court, your gateway to an immersive tennis
-              experience. Get real-time updates, personalize your favorites,
-              stay informed with updated news, and explore player rankings—all
-              in one place. Elevate your love for tennis with us.
+            <p class="feature-description">
+              Get instant updates on match outcomes, scores, and key moments as
+              they happen on the court.
             </p>
+          </div>
+        </v-col>
 
-            <!-- Start Button -->
-            <RouterLink :to="{ name: 'home' }">
-              <button
-                class="text-white px-4 py-2 rounded-full"
-                style="letter-spacing: 0.15em"
-              >
-                Start
-              </button>
-            </RouterLink>
-          </v-col>
-
-          <!-- Image -->
-          <v-col cols="12" md="6" class="d-flex align-center justify-center">
+        <v-col cols="12" md="4">
+          <div class="feature-container">
+            <!--Icon-->
             <img
-              src="@/assets/HeroImage.svg"
-              alt="Imagem"
-              class="w-full h-auto small-image"
+              src="../assets/performanceAnalyse.svg"
+              class="icon-image"
+              style="max-height: 75px"
             />
-          </v-col>
-        </v-row>
-      </v-container>
 
-      <v-container class="mt-10" style="background-color: #efefef">
-        <v-row class="fill-height" style="background-color: #efefef">
-          <v-col cols="12" md="4" style="background-color: #efefef">
-            <div class="feature-container">
-              <!--Icon-->
-              <img src="../assets/liveGames.svg" class="icon-image" />
-
-              <!-- Title -->
-              <h2 class="feature-title">Real-Time Updates</h2>
-
-              <!-- Description -->
-              <p class="feature-description">
-                Get instant updates on match outcomes, scores, and key moments
-                as they happen on the court.
-              </p>
-            </div>
-          </v-col>
-
-          <v-col cols="12" md="4">
-            <div class="feature-container">
-              <!--Icon-->
-              <img
-                src="../assets/performanceAnalyse.svg"
-                class="icon-image"
-                style="max-height: 75px"
-              />
-
-              <!-- Title -->
-              <h2 class="feature-title">Performance Analysis</h2>
-
-              <!-- Description -->
-              <p class="feature-description">
-                Stay informed with concise, powerful data visualizations that
-                elevate your Australian Open experience.
-              </p>
-            </div>
-          </v-col>
-
-          <v-col cols="12" md="4">
-            <div class="feature-container">
-              <!--Icon-->
-              <img
-                src="../assets/followPlayers.svg"
-                class="icon-image"
-                style="max-height: 75px"
-              />
-
-              <!-- Title -->
-              <h2 class="feature-title">Follow Your Players</h2>
-
-              <!-- Description -->
-              <p class="feature-description">
-                Never lose sight of your favorite players. Utilize our tracking
-                feature to follow specific athletes throughout the Australian
-                Open.
-              </p>
-            </div>
-          </v-col>
-        </v-row>
-      </v-container>
-      <v-container
-        class="container mx-auto flex justify-between items-center p-4 mt-10"
-      >
-        <v-row>
-          <v-col cols="12" md="6" class="align-self-center">
             <!-- Title -->
-            <h1 class="text-5xl font-bold mb-4">
-              Elevate your tennis experience.
-            </h1>
+            <h2 class="feature-title">Performance Analysis</h2>
 
             <!-- Description -->
-            <p class="text-lg mb-4">
-              Unlock the power of tennis with <b>Ace Court</b>. Real-time
-              updates, comprehensive player stats, and the ability to favorite
-              games and players redefine your tennis experience. For enthusiasts
-              and strategists alike, our dashboard is the key to a richer, more
-              personalized journey through the world of tennis. Dive in and
-              elevate your game today.
+            <p class="feature-description">
+              Stay informed with concise, powerful data visualizations that
+              elevate your Australian Open experience.
             </p>
-          </v-col>
+          </div>
+        </v-col>
 
-          <!-- Image -->
-          <v-col cols="12" md="6" class="d-flex align-center justify-center">
+        <v-col cols="12" md="4">
+          <div class="feature-container">
+            <!--Icon-->
             <img
-              src="@/assets/Elevate.svg"
-              alt="Imagem"
-              class="w-full h-auto small-image2"
+              src="../assets/followPlayers.svg"
+              class="icon-image"
+              style="max-height: 75px"
             />
-          </v-col>
-        </v-row>
-      </v-container>
-      <v-container class="container mx-auto p-4">
-        <!-- Title -->
-        <h1 class="text-2xl font-bold mb-4">
-          Interactive Experience & <br />Tennis Information
-        </h1>
 
-        <v-row>
-          <v-col cols="12" md="6">
-            <div
-              class="inner-container rounded-md"
-              style="background-color: #efefef"
-            >
-              <img
-                src="@/assets/ranking.svg"
-                alt="Imagem 1"
-                class="inner-image"
-              />
-              <p class="inner-text">Player rankings</p>
-            </div>
-          </v-col>
+            <!-- Title -->
+            <h2 class="feature-title">Follow Your Players</h2>
 
-          <v-col cols="12" md="6">
-            <div
-              class="inner-container rounded-md"
-              style="background-color: #efefef"
-            >
-              <img src="@/assets/vote.svg" alt="Imagem 2" class="inner-image" />
-              <p class="inner-text">Vote the winner</p>
-            </div>
-          </v-col>
-        </v-row>
+            <!-- Description -->
+            <p class="feature-description">
+              Never lose sight of your favorite players. Utilize our tracking
+              feature to follow specific athletes throughout the Australian
+              Open.
+            </p>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container
+      class="container mx-auto flex justify-between items-center p-4 mt-10" style="background-color: #F2F9FC;border-radius: 12px;"
+    >
+      <v-row>
+        <v-col cols="12" md="6" class="align-self-center">
+          <!-- Title -->
+          <h1 class="text-5xl font-bold mb-4">
+            Elevate your tennis experience.
+          </h1>
 
-        <v-row>
-          <v-col cols="12" md="6">
-            <div
-              class="inner-container rounded-md"
-              style="background-color: #efefef"
-            >
-              <img
-                src="@/assets/schedule.svg"
-                alt="Imagem 3"
-                class="inner-image"
-              />
-              <p class="inner-text">Match schedule</p>
-            </div>
-          </v-col>
+          <!-- Description -->
+          <p class="text-lg mb-4">
+            Unlock the power of tennis with <b>Ace Court</b>. Real-time updates,
+            comprehensive player stats, and the ability to favorite games and
+            players redefine your tennis experience. For enthusiasts and
+            strategists alike, our dashboard is the key to a richer, more
+            personalized journey through the world of tennis. Dive in and
+            elevate your game today.
+          </p>
+        </v-col>
 
-          <v-col cols="12" md="6">
-            <div
-              class="inner-container rounded-md"
-              style="background-color: #efefef"
-            >
-              <img
-                src="@/assets/trophy.svg"
-                alt="Imagem 4"
-                class="inner-image"
-              />
-              <p class="inner-text">Win trophies</p>
-            </div>
-          </v-col>
-        </v-row>
+        <!-- Image -->
+        <v-col cols="12" md="6" class="d-flex align-center justify-center">
+          <img
+            src="@/assets/Elevate.svg"
+            alt="Imagem"
+            class="w-full h-auto small-image2"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container class="container mx-auto p-4">
+      <!-- Title -->
+      <h1 class="text-2xl font-bold mb-4">
+        Interactive Experience & <br />Tennis Information
+      </h1>
 
-        <v-row>
-          <v-col cols="12" md="6">
-            <div
-              class="inner-container rounded-md"
-              style="background-color: #efefef"
-            >
-              <img src="@/assets/news.svg" alt="Imagem 5" class="inner-image" />
-              <p class="inner-text">Latest News</p>
-            </div>
-          </v-col>
-
-          <v-col cols="12" md="6">
-            <div
-              class="inner-container rounded-md"
-              style="background-color: #efefef"
-            >
-              <img
-                src="@/assets/stats.svg"
-                alt="Imagem 6"
-                class="inner-image"
-              />
-              <p class="inner-text">See your stats</p>
-            </div>
-          </v-col>
-        </v-row>
-      </v-container>
-      <v-container class="container mx-auto p-4 addColor">
-        <v-carousel hide-delimiters show-arrows>
-          <v-carousel-item>
-            <div class="sub-container">
-              <div class="sub-header">
-                <h3 class="sub-header-text">
-                  "I've been a tennis fanatic for years, and <b>Ace Court</b>
-                  <br />has completely changed the game for me. The real-<br />time
-                  updates and in-depth player insights keep me <br />glued to
-                  the action. It's like having a front-row seat <br />to every
-                  match!"
-                </h3>
-              </div>
-              <div class="sub-content">
-                <img src="@/assets/sara.svg" alt="Imagem 1" class="sub-image" />
-                <div class="sub-texts">
-                  <h2 class="sub-title">Sara Johnson</h2>
-                  <p class="sub-paragraph">Tennis Enthusiastic</p>
-                </div>
-              </div>
-            </div>
-          </v-carousel-item>
-
-          <v-carousel-item>
-            <div class="sub-container">
-              <div class="sub-header">
-                <h3 class="sub-header-text">
-                  "As a sports bettor, Ace Court is my ultimate <br />companion.
-                  Live updates and detailed player stats <br />
-                  give me the edge for strategic bets. The ability to <br />
-                  favorite games and players is a game-changer,<br />
-                  making it effortless to track my key interests. It's a brmust
-                  for serious sports betting."
-                </h3>
-              </div>
-              <div class="sub-content">
-                <img src="@/assets/eric.svg" alt="Imagem 2" class="sub-image" />
-                <div class="sub-texts">
-                  <h2 class="sub-title">Eric May</h2>
-                  <p class="sub-paragraph">Sports Bettor</p>
-                </div>
-              </div>
-            </div>
-          </v-carousel-item>
-        </v-carousel>
-      </v-container>
-      <v-container>
-        <h2 class="feature-title mb-5">
-          Transform your tennis experience with Ace Court. <br />
-          Stay updated with player rankings, schedules, and news in one <br />
-          centralized hub. Elevate your game—explore Ace Court today!
-        </h2>
-        <!-- Start Button -->
-        <RouterLink :to="{ name: 'signup' }">
-          <button
-            class="text-white px-4 py-2 rounded-full mb-10"
-            style="letter-spacing: 0.15em"
+      <v-row>
+        <v-col cols="12" md="6">
+          <div
+            class="inner-container rounded-md"
+            style="background-color: #efefef"
           >
-            Start
-          </button>
-        </RouterLink>
-      </v-container>
-    </main>
+            <img
+              src="@/assets/ranking.svg"
+              alt="Imagem 1"
+              class="inner-image"
+            />
+            <p class="inner-text">Player rankings</p>
+          </div>
+        </v-col>
+
+        <v-col cols="12" md="6">
+          <div
+            class="inner-container rounded-md"
+            style="background-color: #efefef"
+          >
+            <img src="@/assets/vote.svg" alt="Imagem 2" class="inner-image" />
+            <p class="inner-text">Vote the winner</p>
+          </div>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12" md="6">
+          <div
+            class="inner-container rounded-md"
+            style="background-color: #efefef"
+          >
+            <img
+              src="@/assets/schedule.svg"
+              alt="Imagem 3"
+              class="inner-image"
+            />
+            <p class="inner-text">Match schedule</p>
+          </div>
+        </v-col>
+
+        <v-col cols="12" md="6">
+          <div
+            class="inner-container rounded-md"
+            style="background-color: #efefef"
+          >
+            <img src="@/assets/trophy.svg" alt="Imagem 4" class="inner-image" />
+            <p class="inner-text">Win trophies</p>
+          </div>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12" md="6">
+          <div
+            class="inner-container rounded-md"
+            style="background-color: #efefef"
+          >
+            <img src="@/assets/news.svg" alt="Imagem 5" class="inner-image" />
+            <p class="inner-text">Latest News</p>
+          </div>
+        </v-col>
+
+        <v-col cols="12" md="6">
+          <div
+            class="inner-container rounded-md"
+            style="background-color: #efefef"
+          >
+            <img src="@/assets/stats.svg" alt="Imagem 6" class="inner-image" />
+            <p class="inner-text">See your stats</p>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container class="container mx-auto p-4 addColor">
+      <v-carousel hide-delimiters show-arrows>
+        <v-carousel-item>
+          <div class="sub-container">
+            <div class="sub-header">
+              <h3 class="sub-header-text">
+                "I've been a tennis fanatic for years, and <b>Ace Court</b>
+                <br />has completely changed the game for me. The real-<br />time
+                updates and in-depth player insights keep me <br />glued to the
+                action. It's like having a front-row seat <br />to every match!"
+              </h3>
+            </div>
+            <div class="sub-content">
+              <img src="@/assets/sara.svg" alt="Imagem 1" class="sub-image" />
+              <div class="sub-texts">
+                <h2 class="sub-title">Sara Johnson</h2>
+                <p class="sub-paragraph">Tennis Enthusiastic</p>
+              </div>
+            </div>
+          </div>
+        </v-carousel-item>
+
+        <v-carousel-item>
+          <div class="sub-container">
+            <div class="sub-header">
+              <h3 class="sub-header-text">
+                "As a sports bettor, Ace Court is my ultimate <br />companion.
+                Live updates and detailed player stats <br />
+                give me the edge for strategic bets. The ability to <br />
+                favorite games and players is a game-changer,<br />
+                making it effortless to track my key interests. It's a brmust
+                for serious sports betting."
+              </h3>
+            </div>
+            <div class="sub-content">
+              <img src="@/assets/eric.svg" alt="Imagem 2" class="sub-image" />
+              <div class="sub-texts">
+                <h2 class="sub-title">Eric May</h2>
+                <p class="sub-paragraph">Sports Bettor</p>
+              </div>
+            </div>
+          </div>
+        </v-carousel-item>
+      </v-carousel>
+    </v-container>
+    <v-container>
+      <h2 class="feature-title mb-5">
+        Transform your tennis experience with Ace Court. <br />
+        Stay updated with player rankings, schedules, and news in one <br />
+        centralized hub. Elevate your game—explore Ace Court today!
+      </h2>
+      <!-- Start Button -->
+      <RouterLink :to="{ name: 'signup' }">
+        <button
+          class="text-white px-4 py-2 rounded-full mb-10"
+          style="letter-spacing: 0.15em"
+        >
+          Start
+        </button>
+      </RouterLink>
+    </v-container>
     <Footer></Footer>
   </div>
 </template>
@@ -303,7 +292,7 @@ export default {
 <style scoped>
 h1 {
   font-size: 24pt;
-  color: #0f3972;
+  color: #008BCC;
   font-family: "Karla ExtraBold", sans-serif;
 }
 
@@ -427,6 +416,6 @@ button:hover {
 }
 
 .addColor {
-  color: #f2f9fc;
+  color: #F2F9FC;
 }
 </style>
