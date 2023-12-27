cc7dc0b188b48461f0ce77727ab06b96cd94911c
xnm-sdksdk <n.mendonca@protonmail.com>
2023-12-27 16:29:52 +0000

    container

--- src/views/ScoresView.vue
+++ src/views/ScoresView.vue
@@ -1,56 +1,57 @@
 <template>
   <v-app>
     <NavBar></NavBar>
-    <!-- My Games Title -->
-    <div class="container mx-auto flex justify-between items-center p-6">
-      <v-row>
-        <!-- First Column My Games, Live Games -->
-        <v-col cols="9">
-          <div class="py-2">
-            <!-- My Games -->
-            <v-card-title
-              class="rounded-t-xl bg-grey-lighten-4 white--text ma-1 pa-3"
-              >My Games</v-card-title
-            >
-            <v-card
-              class="rounded-b-xl bg-grey-lighten-4 ma-1 pa-4"
-              elevation="3"
-            >
-              <!-- Tournament Stage and Arena -->
-              <div class="py-4">
-                <v-card-text>
-                  <v-row>
-                    <v-col>Australia Open - Quarter Finals</v-col>
-                    <v-col>Rod Laver Arena</v-col>
+    <v-container>
+      <!-- My Games Title -->
+      <div class="container mx-auto flex justify-between items-center p-6">
+        <v-row>
+          <!-- First Column My Games, Live Games -->
+          <v-col cols="9">
+            <div class="py-2">
+              <!-- My Games -->
+              <v-card-title
+                class="rounded-t-xl bg-grey-lighten-4 white--text ma-1 pa-3"
+                >My Games</v-card-title
+              >
+              <v-card
+                class="rounded-b-xl bg-grey-lighten-4 ma-1 pa-4"
+                elevation="3"
+              >
+                <!-- Tournament Stage and Arena -->
+                <div class="py-4">
+                  <v-card-text>
+                    <v-row>
+                      <v-col>Australia Open - Quarter Finals</v-col>
+                      <v-col>Rod Laver Arena</v-col>
+                    </v-row>
+                  </v-card-text>
+
+                  <!-- Header First Game -->
+
+                  <ScoresHeaderResults></ScoresHeaderResults>
+
+                  <!-- First Player - First Game -->
+                  <v-row
+                    class="bg-blue-darken-2 ma-1 pa-1"
+                    v-for="(game, index) in renderingGames"
+                    :key="index"
+                  >
+                    <v-col>
+                      {{ game.event_first_player }}
+                    </v-col>
+                    <!-- <div>{{ game.event_first_player }}</div> -->
+                    <!-- <v-col>{{ game.pointbypoint.points.number_point }}</v-col> -->
+                    <!-- <v-col>{{ getLiveScore(game) }}</v-col>-->
+                    <v-col>{{ getSetResult(game, 1) }}</v-col>
+                    <v-col>{{ getSetResult(game, 2) }}</v-col>
+                    <v-col>{{}}</v-col>
+                    <v-col>{{}}</v-col>
+                    <v-col>{{}}</v-col>
+                    <v-col>{{ game.event_final_result }}</v-col>
                   </v-row>
-                </v-card-text>
 
-                <!-- Header First Game -->
-
-                <ScoresHeaderResults></ScoresHeaderResults>
-
-                <!-- First Player - First Game -->
-                <v-row
-                  class="bg-blue-darken-2 ma-1 pa-1"
-                  v-for="(game, index) in renderingGames"
-                  :key="index"
-                >
-                  <v-col>
-                    {{ game.event_first_player }}
-                  </v-col>
-                  <!-- <div>{{ game.event_first_player }}</div> -->
-                  <!-- <v-col>{{ game.pointbypoint.points.number_point }}</v-col> -->
-                  <!-- <v-col>{{ getLiveScore(game) }}</v-col>-->
-                  <v-col>{{ getSetResult(game, 1) }}</v-col>
-                  <v-col>{{ getSetResult(game, 2) }}</v-col>
-                  <v-col>{{}}</v-col>
-                  <v-col>{{}}</v-col>
-                  <v-col>{{}}</v-col>
-                  <v-col>{{ game.event_final_result }}</v-col>
-                </v-row>
-
-                <!-- Second Player - First Game -->
-                <!-- <v-row class="rounded-b-xl bg-blue-darken-2 ma-1 pa-1">
+                  <!-- Second Player - First Game -->
+                  <!-- <v-row class="rounded-b-xl bg-blue-darken-2 ma-1 pa-1">
                   <v-col></v-col>
                   <v-col>40</v-col>
                   <v-col>6</v-col>
@@ -60,95 +61,95 @@
                   <v-col></v-col>
                   <v-col>1</v-col>
                 </v-row> -->
-              </div>
+                </div>
 
-              <div class="py-4">
-                <!-- Header First Game -->
-                <ScoresHeaderResults></ScoresHeaderResults>
+                <div class="py-4">
+                  <!-- Header First Game -->
+                  <ScoresHeaderResults></ScoresHeaderResults>
 
-                <!-- First Player - First Game -->
-                <v-row class="bg-blue-darken-2 ma-1">
-                  <v-col>N. Djokovic</v-col>
-                  <v-col>40</v-col>
-                  <v-col>6</v-col>
-                  <v-col>0</v-col>
-                  <v-col></v-col>
-                  <v-col></v-col>
-                  <v-col></v-col>
-                  <v-col>1</v-col>
-                </v-row>
+                  <!-- First Player - First Game -->
+                  <v-row class="bg-blue-darken-2 ma-1">
+                    <v-col>N. Djokovic</v-col>
+                    <v-col>40</v-col>
+                    <v-col>6</v-col>
+                    <v-col>0</v-col>
+                    <v-col></v-col>
+                    <v-col></v-col>
+                    <v-col></v-col>
+                    <v-col>1</v-col>
+                  </v-row>
 
-                <!-- Second Player - First Game -->
-                <v-row class="rounded-b-xl bg-blue-darken-2 ma-1">
-                  <v-col>C. Alcaraz</v-col>
-                  <v-col>40</v-col>
-                  <v-col>6</v-col>
-                  <v-col>0</v-col>
-                  <v-col></v-col>
-                  <v-col></v-col>
-                  <v-col></v-col>
-                  <v-col>1</v-col>
-                </v-row>
-              </div>
-            </v-card>
-          </div>
+                  <!-- Second Player - First Game -->
+                  <v-row class="rounded-b-xl bg-blue-darken-2 ma-1">
+                    <v-col>C. Alcaraz</v-col>
+                    <v-col>40</v-col>
+                    <v-col>6</v-col>
+                    <v-col>0</v-col>
+                    <v-col></v-col>
+                    <v-col></v-col>
+                    <v-col></v-col>
+                    <v-col>1</v-col>
+                  </v-row>
+                </div>
+              </v-card>
+            </div>
 
-          <!-- Live Games -->
+            <!-- Live Games -->
 
-          <v-row>
-            <v-col col="9">
-              <div class="py-4">
-                <v-card-title
-                  class="rounded-t-xl bg-grey-lighten-4 white--text ma-1 pa-3"
-                >
-                  Live Games
-                </v-card-title>
-                <v-card
-                  class="rounded-b-xl bg-grey-lighten-4 ma-1 pa-4"
-                  elevation="3"
-                >
-                  <div class="py-4">
-                    <v-card-text>
-                      <v-row>
-                        <v-col>Australia Open - Quarter Finals</v-col>
-                        <v-col>Rod Laver Arena</v-col>
+            <v-row>
+              <v-col col="9">
+                <div class="py-4">
+                  <v-card-title
+                    class="rounded-t-xl bg-grey-lighten-4 white--text ma-1 pa-3"
+                  >
+                    Live Games
+                  </v-card-title>
+                  <v-card
+                    class="rounded-b-xl bg-grey-lighten-4 ma-1 pa-4"
+                    elevation="3"
+                  >
+                    <div class="py-4">
+                      <v-card-text>
+                        <v-row>
+                          <v-col>Australia Open - Quarter Finals</v-col>
+                          <v-col>Rod Laver Arena</v-col>
+                        </v-row>
+                      </v-card-text>
+                      <!-- Fist Game Live Games -->
+                      <ScoresHeaderResults></ScoresHeaderResults>
+
+                      <v-row
+                        v-for="game in liveGames"
+                        :key="game.event_key"
+                        class="bg-blue-darken-2 ma-1"
+                      >
+                        <v-col>{{ game.event_first_player }}</v-col>
+                        <v-col>40</v-col>
+                        <v-col>6</v-col>
+                        <v-col>0</v-col>
+                        <v-col></v-col>
+                        <v-col></v-col>
+                        <v-col></v-col>
+                        <v-col>1</v-col>
                       </v-row>
-                    </v-card-text>
-                    <!-- Fist Game Live Games -->
-                    <ScoresHeaderResults></ScoresHeaderResults>
 
-                    <v-row
-                      v-for="game in liveGames"
-                      :key="game.event_key"
-                      class="bg-blue-darken-2 ma-1"
-                    >
-                      <v-col>{{ game.event_first_player }}</v-col>
-                      <v-col>40</v-col>
-                      <v-col>6</v-col>
-                      <v-col>0</v-col>
-                      <v-col></v-col>
-                      <v-col></v-col>
-                      <v-col></v-col>
-                      <v-col>1</v-col>
-                    </v-row>
+                      <v-row
+                        class="bg-blue-darken-2 ma-1 pa-1 rounded-b-xl"
+                        v-for="(game, index) in renderingGames"
+                        :key="index"
+                      >
+                        <v-col>{{ game.event_second_player }}</v-col>
+                        <v-col>40</v-col>
+                        <v-col>6</v-col>
+                        <v-col>0</v-col>
+                        <v-col></v-col>
+                        <v-col></v-col>
+                        <v-col></v-col>
+                        <v-col>1</v-col>
+                      </v-row>
 
-                    <v-row
-                      class="bg-blue-darken-2 ma-1 pa-1 rounded-b-xl"
-                      v-for="(game, index) in renderingGames"
-                      :key="index"
-                    >
-                      <v-col>{{ game.event_second_player }}</v-col>
-                      <v-col>40</v-col>
-                      <v-col>6</v-col>
-                      <v-col>0</v-col>
-                      <v-col></v-col>
-                      <v-col></v-col>
-                      <v-col></v-col>
-                      <v-col>1</v-col>
-                    </v-row>
-
-                    <!-- Second Player - First Game -->
-                    <!-- <v-row class="rounded-b-xl bg-blue-darken-2 ma-1">
+                      <!-- Second Player - First Game -->
+                      <!-- <v-row class="rounded-b-xl bg-blue-darken-2 ma-1">
                     <v-col>C. Alcaraz</v-col>
                     <v-col>40</v-col>
                     <v-col>6</v-col>
@@ -158,113 +159,111 @@
                     <v-col></v-col>
                     <v-col>1</v-col>
                   </v-row> -->
-                  </div>
-                  <div class="py-4">
-                    <v-card-text>
-                      <v-row>
-                        <v-col>Australia Open - Quarter Finals</v-col>
-                        <v-col>Rod Laver Arena</v-col>
+                    </div>
+                    <div class="py-4">
+                      <v-card-text>
+                        <v-row>
+                          <v-col>Australia Open - Quarter Finals</v-col>
+                          <v-col>Rod Laver Arena</v-col>
+                        </v-row>
+                      </v-card-text>
+                    </div>
+                    <div class="py-4">
+                      <v-card-text>
+                        <v-row>
+                          <v-col>Australia Open - Quarter Finals</v-col>
+                          <v-col>Rod Laver Arena</v-col>
+                        </v-row>
+                      </v-card-text>
+
+                      <!-- Second Game Live Games -->
+                      <ScoresHeaderResults></ScoresHeaderResults>
+
+                      <v-row class="bg-blue-darken-2 ma-1">
+                        <v-col>N. Djokovic</v-col>
+                        <v-col>40</v-col>
+                        <v-col>6</v-col>
+                        <v-col>0</v-col>
+                        <v-col></v-col>
+                        <v-col></v-col>
+                        <v-col></v-col>
+                        <v-col>1</v-col>
                       </v-row>
-                    </v-card-text>
-                  </div>
-                  <div class="py-4">
-                    <v-card-text>
-                      <v-row>
-                        <v-col>Australia Open - Quarter Finals</v-col>
-                        <v-col>Rod Laver Arena</v-col>
+
+                      <!-- Second Player - First Game -->
+                      <v-row class="rounded-b-xl bg-blue-darken-2 ma-1">
+                        <v-col>C. Alcaraz</v-col>
+                        <v-col>40</v-col>
+                        <v-col>6</v-col>
+                        <v-col>0</v-col>
+                        <v-col></v-col>
+                        <v-col></v-col>
+                        <v-col></v-col>
+                        <v-col>1</v-col>
                       </v-row>
-                    </v-card-text>
+                    </div>
+                  </v-card>
+                </div>
+              </v-col>
+            </v-row>
+          </v-col>
 
-                    <!-- Second Game Live Games -->
-                    <ScoresHeaderResults></ScoresHeaderResults>
-
-                    <v-row class="bg-blue-darken-2 ma-1">
-                      <v-col>N. Djokovic</v-col>
-                      <v-col>40</v-col>
-                      <v-col>6</v-col>
-                      <v-col>0</v-col>
-                      <v-col></v-col>
-                      <v-col></v-col>
-                      <v-col></v-col>
-                      <v-col>1</v-col>
-                    </v-row>
-
-                    <!-- Second Player - First Game -->
-                    <v-row class="rounded-b-xl bg-blue-darken-2 ma-1">
-                      <v-col>C. Alcaraz</v-col>
-                      <v-col>40</v-col>
-                      <v-col>6</v-col>
-                      <v-col>0</v-col>
-                      <v-col></v-col>
-                      <v-col></v-col>
-                      <v-col></v-col>
-                      <v-col>1</v-col>
-                    </v-row>
-                  </div>
-                </v-card>
-              </div>
-            </v-col>
-          </v-row>
-        </v-col>
-
-        <!-- Second Column Last Match, Players -->
-        <v-col cols="3">
-          <!-- Last Match -->
-          <v-card-title
-            class="rounded-t-xl bg-blue-lighten-5 white--text ma-1 pa-3"
-          >
-            Last Match
-          </v-card-title>
-          <v-card
-            class="rounded-b-xl bg-grey-lighten-4 ma-1"
-            elevation="3"
-            height="300"
-          >
-            <v-card-text>
-              <!-- First Player - Last Match -->
-              <v-row>
-                <v-col> N. Djokovic (1) </v-col>
-                <v-col>3</v-col>
-              </v-row>
-              <!-- Second Player - Last Match -->
-              <v-row>
-                <v-col> C. Alcaraz (1) </v-col>
-                <v-col>0</v-col>
-              </v-row>
-            </v-card-text>
-          </v-card>
-
-          <div class="">
-            <!-- Players -->
+          <!-- Second Column Last Match, Players -->
+          <v-col cols="3">
+            <!-- Last Match -->
             <v-card-title
               class="rounded-t-xl bg-blue-lighten-5 white--text ma-1 pa-3"
-              >Players</v-card-title
             >
+              Last Match
+            </v-card-title>
             <v-card
               class="rounded-b-xl bg-grey-lighten-4 ma-1"
               elevation="3"
               height="300"
             >
-              <v-data-table :items="players" :key="player_key">
-                <!-- Players -->
-                <v-col>
-                  
-                </v-col>
-
-                <!-- See More Players -->
+              <v-card-text>
+                <!-- First Player - Last Match -->
                 <v-row>
-                  <v-col>
-                    <v-btn @click="seeMorePlayers">See More</v-btn>
-                  </v-col>
+                  <v-col> N. Djokovic (1) </v-col>
+                  <v-col>3</v-col>
                 </v-row>
-              </v-data-table>
+                <!-- Second Player - Last Match -->
+                <v-row>
+                  <v-col> C. Alcaraz (1) </v-col>
+                  <v-col>0</v-col>
+                </v-row>
+              </v-card-text>
             </v-card>
-          </div>
-        </v-col>
-      </v-row>
-    </div>
+
+            <div class="">
+              <!-- Players -->
+              <v-card-title
+                class="rounded-t-xl bg-blue-lighten-5 white--text ma-1 pa-3"
+                >Players</v-card-title
+              >
+              <v-card
+                class="rounded-b-xl bg-grey-lighten-4 ma-1"
+                elevation="3"
+                height="300"
+              >
+                <v-data-table :items="players" :key="player_key">
+                  <!-- Players -->
+                  <v-col> </v-col>
+
+                  <!-- See More Players -->
+                  <v-row>
+                    <v-col>
+                      <v-btn @click="seeMorePlayers">See More</v-btn>
+                    </v-col>
+                  </v-row>
+                </v-data-table>
+              </v-card>
+            </div>
+          </v-col>
+        </v-row>
+      </div>
+    </v-container>
   </v-app>
-  <!-- <div class="container mx-auto flex justify-between items-center p-4"></div> -->
 </template>
 
 <script>
