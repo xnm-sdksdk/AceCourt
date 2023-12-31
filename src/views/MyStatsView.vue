efb782f7bc5a7f5bdad9e65ba29a25b97373cea4
xnm-sdksdk <n.mendonca@protonmail.com>
2023-12-31 01:06:15 +0000

    my stats layout

--- src/views/MyStatsView.vue
+++ src/views/MyStatsView.vue
@@ -1,11 +1,25 @@
 <template>
-  <NavBar></NavBar>
-  <div class="">
-    <h1>My Stats</h1>
-  </div>
-  <div class="">
-    <ProfileButtons></ProfileButtons>
-  </div>
+  <v-app>
+    <NavBar></NavBar>
+    <v-container>
+      <v-main>
+        <div class="font-bold">
+          <h1>My Profile</h1>
+        </div>
+        <div class="ma-3 pa-3">
+          <ProfileButtons></ProfileButtons>
+        </div>
+        <div class="container mx-auto flex justify-between items-center p-4">
+          <v-row class="justify-center align-center">
+            <v-col cols="11" class="">
+              <v-card class="rounded-lg bg-grey-lighten-5" elevation="3">
+              </v-card>
+            </v-col>
+          </v-row>
+        </div>
+      </v-main>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
