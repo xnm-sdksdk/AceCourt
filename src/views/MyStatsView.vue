989a6448e096ae5a01a79e5f4a1695197a58c603
xnm-sdksdk <n.mendonca@protonmail.com>
2024-01-01 21:38:44 +0000

    row cols setup my stats

--- src/views/MyStatsView.vue
+++ src/views/MyStatsView.vue
@@ -12,8 +12,71 @@
         <div class="container mx-auto flex justify-between items-center p-4">
           <v-row class="justify-center align-center">
             <v-col cols="11" class="">
-              <v-card class="rounded-lg bg-grey-lighten-5" elevation="3">
+              <v-card
+                class="rounded-lg bg-grey-lighten-5"
+                elevation="3"
+                height="300"
+              >
+                <v-row class="justify-center align-center">
+                  <v-col cols="11">
+                    <p>Graph Hours per Day</p>
+                  </v-col>
+                </v-row>
               </v-card>
+              <!-- Second Row -->
+              <v-row class="justify-center align-center ma-4">
+                <v-col cols="6">
+                  <v-card
+                    class="rounded-lg bg-grey-lighten-5"
+                    elevation="3"
+                    height="100"
+                  >
+                    <p>Days in streak</p>
+                  </v-card>
+                </v-col>
+                <v-col cols="6">
+                  <v-card
+                    class="rounded-lg bg-grey-lighten-5"
+                    elevation="3"
+                    height="100"
+                  >
+                    <p>Average Hours</p>
+                  </v-card>
+                </v-col>
+              </v-row>
+              <!-- Third Row -->
+              <v-row class="justify-center align-center">
+                <v-col cols="6">
+                  <v-card
+                    class="rounded-lg bg-grey-lighten-5"
+                    elevation="3"
+                    height="100"
+                  >
+                    <p>Right Games</p>
+                  </v-card>
+                </v-col>
+                <v-col cols="6">
+                  <v-card
+                    class="rounded-lg bg-grey-lighten-5"
+                    elevation="3"
+                    height="100"
+                  >
+                    <p>Right Games Streak</p>
+                  </v-card>
+                </v-col>
+              </v-row>
+              <!-- Fourth Row -->
+              <v-row class="justify-center align-center">
+                <v-col cols="12">
+                  <v-card
+                    class="rounded-lg bg-grey-lighten-5"
+                    elevation="3"
+                    height="100"
+                  >
+                    <p>Favorite Players</p>
+                  </v-card>
+                </v-col>
+              </v-row>
             </v-col>
           </v-row>
         </div>
