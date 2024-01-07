cb80936e51c473b4b25378295b1504b01c479c87
FredSilva21 <fredsilva43@gmail.com>
2024-01-07 14:25:38 +0000

    adjust sign up form

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -4,82 +4,79 @@
       <div class="max-w-xl mx-auto bg-white rounded p-8 shadow-md">
         <h2 class="text-2xl font-semibold mb-4 flex justify-center">Sign Up</h2>
 
-        <!-- Form -->
-        <form>
-          <!-- Name Input -->
-          <div class="mb-4">
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Name"
-              class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring focus:border-blue-500"
-              v-model.trim="name"
-            />
-          </div>
+        <!-- Name Input -->
+        <div class="mb-4">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring focus:border-blue-500"
+            v-model.trim="name"
+          />
+        </div>
 
-          <!-- Email Input -->
-          <div class="mb-4">
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Email"
-              class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring focus:border-blue-500"
-              v-model.trim="email"
-            />
-          </div>
+        <!-- Email Input -->
+        <div class="mb-4">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring focus:border-blue-500"
+            v-model.trim="email"
+          />
+        </div>
 
-          <!-- Password Input -->
-          <div class="mb-4 relative">
-            <input
-              id="password"
-              name="password"
-              placeholder="Password"
-              class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring focus:border-blue-500"
-              :type="showPassword ? 'text' : 'password'"
-              v-model.trim="password"
-            />
-            <div
-              class="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
-              @click="togglePasswordVisibility"
-            >
-              <img v-if="!showPassword" src="../assets/seePass.svg" alt="" />
-              <img v-if="showPassword" src="../assets/hidePass.svg" alt="" />
-            </div>
-          </div>
-
-          <!-- Type of User Select -->
-          <div class="mb-4">
-            <!-- Type of User Select -->
-            <div class="mb-4 relative">
-              <select
-                id="userType"
-                name="userType"
-                class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring focus:border-blue-500"
-                v-model="typeUser"
-              >
-                <option value="" disabled selected>Type of user</option>
-                <option value="Tennis Enthusiast">Tennis Enthusiast</option>
-                <option value="Sports Bettor">Sports Bettor</option>
-                <option value="Youth Player">Youth Player</option>
-              </select>
-              <div
-                class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none"
-              >
-                <img src="@/assets/arrowSelect.svg" alt="" />
-              </div>
-            </div>
-          </div>
-
-          <!-- Button Sign Up -->
-          <button
-            type="submit"
-            class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-700"
+        <!-- Password Input -->
+        <div class="mb-4 relative">
+          <input
+            id="password"
+            name="password"
+            placeholder="Password"
+            class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring focus:border-blue-500"
+            :type="showPassword ? 'text' : 'password'"
+            v-model.trim="password"
+          />
+          <div
+            class="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+            @click="togglePasswordVisibility"
           >
-            Sign Up
-          </button>
-        </form>
+            <img v-if="!showPassword" src="../assets/seePass.svg" alt="" />
+            <img v-if="showPassword" src="../assets/hidePass.svg" alt="" />
+          </div>
+        </div>
+
+        <!-- Type of User Select -->
+        <div class="mb-4">
+          <!-- Type of User Select -->
+          <div class="mb-4 relative">
+            <select
+              id="userType"
+              name="userType"
+              class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring focus:border-blue-500"
+              v-model="typeUser"
+            >
+              <option value="" disabled selected>Type of user</option>
+              <option value="Tennis Enthusiast">Tennis Enthusiast</option>
+              <option value="Sports Bettor">Sports Bettor</option>
+              <option value="Youth Player">Youth Player</option>
+            </select>
+            <div
+              class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none"
+            >
+              <img src="@/assets/arrowSelect.svg" alt="" />
+            </div>
+          </div>
+        </div>
+
+        <!-- Button Sign Up -->
+        <button
+          type="submit"
+          class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-700"
+        >
+          Sign Up
+        </button>
 
         <!-- Linha "Or" -->
         <div class="my-6 flex items-center">
@@ -102,7 +99,7 @@
 </template>
 
 <script>
-import {useUserStore} from "@/stores/user.js"
+import { useUserStore } from "@/stores/user.js";
 export default {
   data() {
     return {
@@ -111,7 +108,7 @@ export default {
       email: "",
       password: "",
       typeUser: "",
-      userStore:useUserStore()
+      userStore: useUserStore(),
     };
   },
 
@@ -121,10 +118,15 @@ export default {
     },
     createAccount() {
       try {
-        this.userStore.register(this.name,this.email,this.password,this.typeUser)
-        this.$router.push({name:"login"})
+        this.userStore.register(
+          this.name,
+          this.email,
+          this.password,
+          this.typeUser
+        );
+        this.$router.push({ name: "login" });
       } catch (error) {
-        alert(`Error: ${error.message}` )
+        alert(`Error: ${error.message}`);
       }
     },
   },
