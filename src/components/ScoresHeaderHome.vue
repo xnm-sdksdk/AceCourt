aa4f5d7e2271c88409c929540740f05a2c7df03d
xnm-sdksdk <n.mendonca@protonmail.com>
2024-01-08 16:20:31 +0000

    component highlight game

---
+++ src/components/ScoresHeaderHome.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-blue-900 font-bold">
+    <v-row class="rounded-t-xl ma-1">
+      <v-col>Sets</v-col>
+      <v-col>1st</v-col>
+      <v-col>2nd</v-col>
+      <v-col>3rd</v-col>
+      <v-col>4th</v-col>
+      <v-col>5th</v-col>
+      <v-col>Final</v-col>
+    </v-row>
+  </div>
+</template>
