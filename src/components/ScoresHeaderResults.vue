bd95310fe9ff6cb47ffae63e07a5a509ba4d6c5a
xnm-sdksdk <n.mendonca@protonmail.com>
2024-01-12 18:39:50 +0000

    data alignment

--- src/components/ScoresHeaderResults.vue
+++ src/components/ScoresHeaderResults.vue
@@ -1,13 +1,13 @@
 <template>
   <v-row class="rounded-t-xl bg-blue-lighten-5 ma-1" v-if="game">
     <div class="flex text-blue-900">
-      <v-col>{{ game.tournament_round }}</v-col>
-      <v-col>1st</v-col>
-      <v-col>2nd</v-col>
-      <v-col>3rd</v-col>
-      <v-col>4th</v-col>
-      <v-col>5th</v-col>
-      <v-col>
+      <v-col cols="8">{{ game.tournament_round }}</v-col>
+      <v-col cols="2">1st</v-col>
+      <v-col cols="2">2nd</v-col>
+      <v-col cols="2">3rd</v-col>
+      <v-col cols="2">4th</v-col>
+      <v-col cols="2">5th</v-col>
+      <v-col cols="3">
         <img src="../assets/Live.svg" />
       </v-col>
     </div>
