5e22f0b1f34759dffc0743b7bfe140e083dbf4a0
xnm-sdksdk <n.mendonca@protonmail.com>
2024-01-13 12:51:36 +0000

    fixing positioning

--- src/components/ScoresHeaderResults.vue
+++ src/components/ScoresHeaderResults.vue
@@ -1,13 +1,13 @@
 <template>
   <v-row class="rounded-t-xl bg-blue-lighten-5 ma-1" v-if="game">
-    <div class="flex text-blue-900">
-      <v-col cols="8">{{ game.tournament_round }}</v-col>
-      <v-col cols="2">1st</v-col>
-      <v-col cols="2">2nd</v-col>
-      <v-col cols="2">3rd</v-col>
-      <v-col cols="2">4th</v-col>
-      <v-col cols="2">5th</v-col>
-      <v-col cols="3">
+    <div class="flex text-blue-900 max-w-xl">
+      <v-col cols="6">{{ game.tournament_round }}</v-col>
+      <v-col cols="1" class="ml-4 px-1">1st</v-col>
+      <v-col cols="1" class="ml-4 px-1">2nd</v-col>
+      <v-col cols="1" class="ml-4 px-1">3rd</v-col>
+      <v-col cols="1" class="ml-4 px-1">4th</v-col>
+      <v-col cols="1" class="ml-4 px-1">5th</v-col>
+      <v-col cols="1">
         <img src="../assets/Live.svg" />
       </v-col>
     </div>
