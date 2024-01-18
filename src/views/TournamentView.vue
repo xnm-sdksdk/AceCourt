b275275a944f0da25d4ee047381ed0b7822613d9
xnm-sdksdk <n.mendonca@protonmail.com>
2024-01-18 04:20:39 +0000

    organize layout

--- src/views/TournamentView.vue
+++ src/views/TournamentView.vue
@@ -1,66 +1,68 @@
 <template>
   <NavBar></NavBar>
   <v-container class="containerTournament">
-    <v-row>
-      <v-col cols="4">
-        <router-link :to="{ name: 'schedule' }">
-          <v-row class="tournamentCont">
-            <v-col>
-              <v-row>
-                <h1>Schedule</h1>
-              </v-row>
-              <v-row>
-                <p>
-                  Explore the comprehensive schedule feature on our tennis
-                  dashboard, keeping you updated on match timings, player
-                  matchups, and tournament progress in real-time.
-                </p>
-              </v-row>
-            </v-col>
-          </v-row>
-        </router-link>
-      </v-col>
+    <div class="flex justify-center items-center">
+      <v-row>
+        <v-col class="mx-3">
+          <router-link :to="{ name: 'schedule' }">
+            <v-row class="tournamentCont">
+              <v-col>
+                <v-row>
+                  <h1>Schedule</h1>
+                </v-row>
+                <v-row>
+                  <p>
+                    Explore the comprehensive schedule feature on our tennis
+                    dashboard, keeping you updated on match timings, player
+                    matchups, and tournament progress in real-time.
+                  </p>
+                </v-row>
+              </v-col>
+            </v-row>
+          </router-link>
+        </v-col>
 
-      <v-col cols="4">
-        <router-link :to="{ name: 'draw' }">
-          <v-row class="tournamentCont">
-            <v-col>
-              <v-row>
-                <h1>Draws</h1>
-              </v-row>
-              <v-row>
-                <p>
-                  Explore the world of tennis with our comprehensive dashboard
-                  for live draws, offering up-to-the-minute updates and detailed
-                  information, ensuring you stay connected to the excitement of
-                  the game.
-                </p>
-              </v-row>
-            </v-col>
-          </v-row>
-        </router-link>
-      </v-col>
+        <v-col class="mx-3">
+          <router-link :to="{ name: 'draw' }">
+            <v-row class="tournamentCont">
+              <v-col>
+                <v-row>
+                  <h1>Draws</h1>
+                </v-row>
+                <v-row>
+                  <p>
+                    Explore the world of tennis with our comprehensive dashboard
+                    for live draws, offering up-to-the-minute updates and
+                    detailed information, ensuring you stay connected to the
+                    excitement of the game.
+                  </p>
+                </v-row>
+              </v-col>
+            </v-row>
+          </router-link>
+        </v-col>
 
-      <v-col cols="4">
-        <router-link :to="{ name: 'ranking' }">
-          <v-row class="tournamentCont">
-            <v-col>
-              <v-row>
-                <h1>Ranking</h1>
-              </v-row>
-              <v-row>
-                <p>
-                  Explore our tennis dashboard, meticulously designed for avid
-                  fans, offering real-time data, player statistics, and match
-                  insights to enhance your tennis experience and keep you on top
-                  of the rankings.
-                </p>
-              </v-row>
-            </v-col>
-          </v-row>
-        </router-link>
-      </v-col>
-    </v-row>
+        <v-col class="mx-3">
+          <router-link :to="{ name: 'ranking' }">
+            <v-row class="tournamentCont">
+              <v-col>
+                <v-row>
+                  <h1>Ranking</h1>
+                </v-row>
+                <v-row>
+                  <p>
+                    Explore our tennis dashboard, meticulously designed for avid
+                    fans, offering real-time data, player statistics, and match
+                    insights to enhance your tennis experience and keep you on
+                    top of the rankings.
+                  </p>
+                </v-row>
+              </v-col>
+            </v-row>
+          </router-link>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
 <script>
