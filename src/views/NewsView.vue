7c2084113aef381f1d493699758f8eeee447611d
xnm-sdksdk <n.mendonca@protonmail.com>
2024-01-18 05:16:33 +0000

    content

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -4,15 +4,15 @@
     <v-row>
       <!-- Coluna para o texto (ocupando 3 colunas em telas médias e grandes) -->
       <v-col cols="12" md="3">
-        <h1 class="text-5xl font-bold mb-4">
+        <h1 class="text-5xl font-bold mb-4 mx-3">
           Discover Australian Open Like Never Before
         </h1>
 
-        <p class="text-lg mb-4">
-          Welcome to Ace Court, your gateway to an immersive tennis
-          experience. Get real-time updates, personalize your favorites, stay
-          informed with updated news, and explore player rankings—all in one
-          place. Elevate your love for tennis with us.
+        <p class="flex text-lg mb-4 m-4 py-3 max-w-md">
+          Welcome to Ace Court, your gateway to an immersive tennis experience.
+          Get real-time updates, personalize your favorites, stay informed with
+          updated news, and explore player rankings—all in one place. Elevate
+          your love for tennis with us.
         </p>
 
         <RouterLink :to="{ name: 'home' }">
@@ -34,6 +34,67 @@
         />
       </v-col>
     </v-row>
+    <div class="my-5 py-5 justify-center align-center">
+      <v-row>
+        <v-col>
+          <v-card class="mx-auto" max-width="344">
+            <v-img
+              src="https://imgs.search.brave.com/6irUKBwEUV5qZ8DcT_SsyOtTR-WaDb_Gy6JM_r59K38/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuaW5kZXBlbmRl/bnQuY28udWsvMjAy/My8wOS8wOS8xOC8w/OS01NWFkMjI3MmE0/N2Y0MGZmOGNhMzY1/YWRmNjY5Y2NkMi5q/cGc_cXVhbGl0eT03/NSZ3aWR0aD02NDAm/Y3JvcD0zOjIsc21h/cnQmYXV0bz13ZWJw"
+              height="200px"
+              cover
+            ></v-img>
+
+            <v-card-title> Medvedev gets revenge </v-card-title>
+
+            <v-card-text>
+              I'm a thing. But, like most politicians, he promised more than he
+              could deliver. You won't have time for sleeping, soldier, not with
+              all the bed making you'll be doing. Then we'll go with that data
+              file! Hey, you add a one and two zeros to that or we walk! You're
+              going to do his laundry? I've got to find a way to escape.
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="mx-auto" max-width="344">
+            <v-img
+              src="https://imgs.search.brave.com/6irUKBwEUV5qZ8DcT_SsyOtTR-WaDb_Gy6JM_r59K38/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuaW5kZXBlbmRl/bnQuY28udWsvMjAy/My8wOS8wOS8xOC8w/OS01NWFkMjI3MmE0/N2Y0MGZmOGNhMzY1/YWRmNjY5Y2NkMi5q/cGc_cXVhbGl0eT03/NSZ3aWR0aD02NDAm/Y3JvcD0zOjIsc21h/cnQmYXV0bz13ZWJw"
+              height="200px"
+              cover
+            ></v-img>
+
+            <v-card-title> Medvedev gets revenge </v-card-title>
+
+            <v-card-text>
+              I'm a thing. But, like most politicians, he promised more than he
+              could deliver. You won't have time for sleeping, soldier, not with
+              all the bed making you'll be doing. Then we'll go with that data
+              file! Hey, you add a one and two zeros to that or we walk! You're
+              going to do his laundry? I've got to find a way to escape.
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="mx-auto" max-width="344">
+            <v-img
+              src="https://imgs.search.brave.com/6irUKBwEUV5qZ8DcT_SsyOtTR-WaDb_Gy6JM_r59K38/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuaW5kZXBlbmRl/bnQuY28udWsvMjAy/My8wOS8wOS8xOC8w/OS01NWFkMjI3MmE0/N2Y0MGZmOGNhMzY1/YWRmNjY5Y2NkMi5q/cGc_cXVhbGl0eT03/NSZ3aWR0aD02NDAm/Y3JvcD0zOjIsc21h/cnQmYXV0bz13ZWJw"
+              height="200px"
+              cover
+            ></v-img>
+
+            <v-card-title> Medvedev gets revenge </v-card-title>
+
+            <v-card-text>
+              I'm a thing. But, like most politicians, he promised more than he
+              could deliver. You won't have time for sleeping, soldier, not with
+              all the bed making you'll be doing. Then we'll go with that data
+              file! Hey, you add a one and two zeros to that or we walk! You're
+              going to do his laundry? I've got to find a way to escape.
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
 
@@ -50,7 +111,7 @@ export default {
 <style>
 h1 {
   font-size: 24pt;
-  color: #008BCC;
+  color: #008bcc;
   font-family: "Karla ExtraBold", sans-serif;
 }
 
